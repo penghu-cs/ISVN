15a836f44ddcda08c5d4993878b8ba68a7fcc7dd
penghu-cs <45786915+penghu-cs@users.noreply.github.com>
2021-12-30 00:09:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ parser.add_argument('--alpha', type=float, default=0.5)
 parser.add_argument('--threshold', type=float, default=0.7)  # MNIST_SVHN: 0.9 INRIA-Websearch 0.7
 parser.add_argument('--beta', type=float, default=1e-2)  # 400: 8e-2 1000: 3e-2 2000: 2e-2 3000: 2e-2 4000: 2e-2
 parser.add_argument('--view_id', type=int, default=1)
-parser.add_argument('--epochs', type=int, default=100)
-parser.add_argument('--K', type=int, default=2000) # 400 1000 2000 3000 4000
+parser.add_argument('--epochs', type=int, default=200)
+parser.add_argument('--K', type=int, default=400) # 400 1000 2000 3000 4000
 parser.add_argument('--CNN', action='store_true', default=False)
 parser.add_argument('--multiprocessing', action='store_true', default=False)
 parser.add_argument('--datasets', nargs='+', help='<Required> Quantization bits', default='INRIA-Websearch') # nus_wide xmedianet INRIA-Websearch MNIST SVHN
