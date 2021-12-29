a13b20f0321fbee479e8fd7e7890298c49a7ca0d
penghu-cs <45786915+penghu-cs@users.noreply.github.com>
2021-12-29 22:29:30 +0800

    Update train_ISVN.sh

--- train_ISVN.sh
+++ train_ISVN.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 output_shape=1024
 gama=1
-datasets="MNIST SVHN" #nus_wide xmedianet INRIA-Websearch MNIST SVHN
+datasets="xmedianet" #nus_wide xmedianet INRIA-Websearch MNIST SVHN
 batch_size=16
-beta=0.5
-alpha=8e-2
-threshold=0.9
-seed=0
+alpha=0.5
+beta=1e-2
+threshold=0.7
+seed=1024
 K=400
 epochs=200
 
 # train View 0
 python train_ISVN.py --datasets $datasets --view_id 0 --seed $seed --epochs $epochs --batch_size $batch_size --output_shape $output_shape --beta $beta --alpha $alpha --threshold $threshold --K $K --gpu_id 0 &
-
 # train View 1
 python train_ISVN.py --datasets $datasets --view_id 1 --seed $seed --epochs $epochs --batch_size $batch_size --output_shape $output_shape --beta $beta --alpha $alpha --threshold $threshold --K $K --gpu_id 1 &
 
