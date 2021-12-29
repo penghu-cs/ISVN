c70a1bedaf9c30c72bc853778bdd77a15e939b36
penghu-cs <45786915+penghu-cs@users.noreply.github.com>
2021-12-29 09:59:37 +0800

    Update train_ISVN.sh

--- train_ISVN.sh
+++ train_ISVN.sh
@@ -10,12 +10,14 @@ seed=0
 K=400
 epochs=200
 
+# train View 0
+python train_ISVN.py --datasets $datasets --view_id 0 --seed $seed --epochs $epochs --batch_size $batch_size --output_shape $output_shape --beta $beta --alpha $alpha --threshold $threshold --K $K --gpu_id 0 &
+
 # train View 1
 python train_ISVN.py --datasets $datasets --view_id 1 --seed $seed --epochs $epochs --batch_size $batch_size --output_shape $output_shape --beta $beta --alpha $alpha --threshold $threshold --K $K --gpu_id 1 &
-# train View 0
-python train_ISVN.py --datasets $datasets --view_id 0 --seed $seed --epochs $epochs --batch_size $batch_size --output_shape $output_shape --beta $beta --alpha $alpha --threshold $threshold --K $K --gpu_id 0
 
 # python train_ISVN.py --view_id -1 --datasets $datasets --seed $seed --epochs $epochs --batch_size $batch_size --output_shape $output_shape --beta $beta --alpha $alpha --K $K --gpu_id -1 --multiprocessing
 
+wait
 # eval
 python train_ISVN.py --mode eval --datasets $datasets --view -1 --output_shape $output_shape --beta $beta --alpha $alpha --threshold $threshold --K $K --gpu_id 0 --num_workers 0
